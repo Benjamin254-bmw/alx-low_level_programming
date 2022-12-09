ea3a4daa9df3a0e99588cbddd73d5430f569c74b
Benjamin254-bmw <wachirabenjamin18@gmail.com>
2022-12-09 06:23:59 -0800

    adding the two numbers stored in the environment variables WATER and STIR and prints the result

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
-#include < stdio.h >
+#include <stdio.h>
+
 /**
  * main - Entry Point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ *Description: prints "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
  */
