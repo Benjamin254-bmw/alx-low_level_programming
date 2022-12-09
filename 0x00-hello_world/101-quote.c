fcaf6c45c8854cc512ccd172dd365b0bd321a68a
Benjamin254-bmw <wachirabenjamin18@gmail.com>
2022-12-09 07:38:09 -0800

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print Quote
+ *
+ * Description: prints Doras quote
+ *
+ * Return: 1 (Success)
+ */
+
+int main(void)
+{
+	putchar("\"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
