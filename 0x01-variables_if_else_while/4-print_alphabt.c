4cfd1a0ead9554d3b15a38c191ffb49fc58a8cd6
Benjamin254-bmw <wachirabenjamin18@gmail.com>
2022-12-09 14:15:14 -0800

    Print all the letters except q and e in lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 /**
- * Main - Print the alphabet in lowercase
+ * main - Print the alphabet in lowercase
  *
- * Description -
+ * Description - Print all the letters except q and e4
  *
- *
+ * Return: Always 0 (Success)
  */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
