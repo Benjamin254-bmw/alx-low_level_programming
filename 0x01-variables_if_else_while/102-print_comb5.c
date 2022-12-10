69dfea7fbb83ffb5d16384ee53594a4a1ea5ef40
Benjamin254-bmw <wachirabenjamin18@gmail.com>
2022-12-09 18:15:13 -0800

    program that prints all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 /**
- * main - print the letters of the alphabet
+ * main - Entry point, Prints all possible combinations of a pair of two digit numbers, without any repetition
  *
- * Description: print the letters of the alphabet except e, q
+ * Description: numbers should range from 0 to 99
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int i = 0;
+	int a, b;
 
-	while (i < 48)
+	for (a = 0; a <= 98; a++)
 	{
-		if (i < 10)
-			putchar(i + '0');
-		else if (i > 41)
-			putchar(i - 10 + 'A');
-		i++;
+		for (b = 1; b <= 99; b++)
+		{
+		if (a < b && a != b)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (!(a == 98 && b == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
-	putchar(10);
-
-	return (0);
+}
+putchar('\n');
+return (0);
 }
