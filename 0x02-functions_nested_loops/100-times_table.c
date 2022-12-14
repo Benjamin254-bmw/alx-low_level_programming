5fcf7590fc2e265021687d489e641b606793c141
Benjamin254-bmw <wachirabenjamin18@gmail.com>
2022-12-14 15:06:03 -0800

    unction that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,7 +35,7 @@ void print_times_table(int n)
 					_putchar((product / 10) % 10 + '0');
 					_putchar(product % 10 + '0');
 				}
-				else if (product > 99 &&, 100)
+				else if (product > 99 && 100)
 				{
 					_putchar(',');
 					_putchar(' ');
