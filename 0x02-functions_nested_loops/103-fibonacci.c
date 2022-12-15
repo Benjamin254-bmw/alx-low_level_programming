1aa1762f02a5a658f3b6b6ea5ab379f6b3e12e88
Benjamin254-bmw <wachirabenjamin18@gmail.com>
2022-12-14 18:10:08 -0800

    program that finds and prints the sum of the even-valued terms under 4000000, followed by a new line

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main -finds and prints the sum of the even-valued terms below 4,000,000
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	int long total_sum, sum, first, second;
+
+	total_sum = 0;
+	sum = 0;
+	first = 0;
+	second = 1;
+
+	while
+	(sum < 4000000);
+	{
+		sum = first + second;
+		while (sum % 2 = 0)
+		{
+			total_sum += sum;
+		}
+		first = second;
+		second = sum;
+	}
+
+	printf("li\n", total_sum);
+
+	return (0);
+}
