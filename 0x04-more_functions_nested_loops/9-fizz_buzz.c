0c97dcc79f61f9ccc2aea8e37f519ae3cc2cbfb5
Benjamin254-bmw <wachirabenjamin18@gmail.com>
2022-12-16 01:52:16 -0800

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - take the fizzbuzz challenge
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n < 100; n++)
+	{
+		if (n % 5 == 0 && n % 3 == 0)
+			printf("FizzBuzz ");
+		else if (n % 5 == 0)
+			if (n == 100)
+			printf("Buzz");
+			else
+				printf("Buzz ");
+		else if (n % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", n);
+	}
+	printf("\n");
+
+	return (0);
+}
