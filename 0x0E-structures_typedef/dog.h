e514bfd574aa22280ab7076ce8c74044748ac686
Benjamin254-bmw <wachirabenjamin18@gmail.com>
2023-01-17 07:40:28 -0800

    Defined a new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,33 +1,24 @@
 #ifndef DOG_H
 #define DOG_H
 
+int _putchar(char c);
+
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
- *
- * Description: Longer description
+ * struct dog - best friend of humankind
+ * @name: doggo's name
+ * @age: doggo's age
+ * @owner: owner's name
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
-#endif
+#endif /* !DOG_H */
