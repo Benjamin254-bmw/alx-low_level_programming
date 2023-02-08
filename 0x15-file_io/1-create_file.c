3b63e1c53090654fb7dc8e2663036b8da452d521
Benjamin254-bmw <wachirabenjamin18@gmail.com>
2023-02-08 13:30:02 -0800

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -32,6 +32,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	unsigned int length;
 	ssize_t bytes_written;
+
 	if (filename == NULL)
 	return (-1);
 		fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
