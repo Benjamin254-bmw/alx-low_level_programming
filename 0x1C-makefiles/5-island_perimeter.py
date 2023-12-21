124c3a7d0464b9c18fda5b6061d3d7f29e7a7dea
Benjamin <77714403+Benjamin254-bmw@users.noreply.github.com>
2023-12-21 17:00:37 +0300

    Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,38 +1,31 @@
 #!/usr/bin/python3
-"""Defines an island perimeter module."""
+"""Defines Island perimeter module"""
 
 
 def adjason(up, down, left, right):
-    """returns the number of unit adjason with"""
+    """the function returns the number of unit adjason with"""
     count = up + down + left + right
     return 4 - count
 
+
 def island_perimeter(grid):
-"""Return the perimiter of an island.
-
-    The grid represents water by 0 and land by 1.
-
-    Args:
-        grid (list): A list of list of integers representing an island.
-    Returns:
-        The perimeter of the island defined in grid.
-    """
-   perimeter = 0
-   for row in range(len(grid)):
-       for j in range(len(grid[row])):
-           if grid[row][j]:
-               up = 0
-               left = 0
-               right = 0
-               down = 0
+    """function returns island perimeter"""
+    perimeter = 0
+    for row in range(len(grid)):
+        for j in range(len(grid[row])):
+            if grid[row][j]:
+                up = 0
+                left = 0
+                right = 0
+                down = 0
                 if row:
-                   up = grid[row-1][j]
+                    up = grid[row-1][j]
                 if row != len(grid)-1:
                     down = grid[row+1][j]
                 if j:
                     left = grid[row][j-1]
                 if j != len(grid[row])-1:
                     right = grid[row][j+1]
-                cent = adjason(up, down, left, right)
-                perimeter += cent
-   return perimeter
+                cnt = adjason(up, down, left, right)
+                perimeter += cnt
+    return perimeter
