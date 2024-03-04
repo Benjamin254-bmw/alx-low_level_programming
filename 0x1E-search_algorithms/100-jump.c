ca1e73161e3354f69a3651c6acf4a1233b2baa6e
Benjamin wachira <wachirabenjamin18@gmail.com>
2024-03-04 10:43:11 +0300

    Jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,12 +14,13 @@
 int jump_search(int *array, size_t size, int value)
 {
 	int step, index, prev;
+
 	if (array == NULL || size == 0)
 		return (-1);
 
 	step = (int)sqrt(size);
 	prev = index = 0;
-	
+
 	do {
 		printf("Value checked array[%d] = [%d]\n", index, array[index]);
 
@@ -31,7 +32,7 @@ int jump_search(int *array, size_t size, int value)
 	} while (index < (int)size && array[index] < value);
 
 	printf("Value found between indexes [%d] and [%d]\n", prev, index);
-	
+
 	for (; prev <= index && prev < (int)size; prev++)
 	{
 		printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
